357f1e611f104157b00d9247b2c72b745876a4b6
Derek Lee <dereklee@Dereks-MacBook-Air.local>
2024-07-02 18:37:52 +0800

    fixed bugs

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='assets')
 
 @app.route("/")
 def home():
